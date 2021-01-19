f7a194a3da586ab312c8003b1e53c79663473516
Katherine-fe <kattyserranobasan@gmail.com>
2021-01-19 10:40:02 -0500

    comentario de prueba

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,65 @@
 const cipher = {
-  // ...
-};
+  encode: (toEncode, offSet) => {
+    let codAscii; /*Almacena el codigo Ascii despues de aplicar la formula*/
+    let codCifrado; /*Convierte el codigo Ascii al Alfabeto normal*/
+    let finCifrado = ""; /*Almacena el resultado*/
+  
+    if (toEncode == "" || offSet == "") {  //valida si algun campo se encuentra vacio
+      alert("Ingresa el Texto a Cifrar o el desplazamiento");
+    }
 
-export default cipher;
+    for (let i = 0; i <= toEncode.length; i++) { //bucle 
+      let toAscii = toEncode.charCodeAt(i);
+  
+      if (toAscii >=65  && toAscii <= 90) {
+        codAscii = (toAscii - 65 + offSet) % 26 + 65;
+        codCifrado = String.fromCharCode(codAscii);
+        finCifrado += codCifrado;
+      }
+      
+      else if(toAscii >=0  && toAscii <= 64){
+        finCifrado = "";
+        alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'");
+        
+      }
+      else if(toAscii >=123  && toAscii <= 255){
+        finCifrado = "";
+        alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'");
+        
+      }   
+      
+        }return finCifrado
+  },
+    
+  decode: (toDecode, offSet) => {
+    let codAscii; /*Almacena el codigo Ascii despues de aplicar la formula*/
+    let codDescifrado; /*Convierte el codigo Ascii al Alfabeto normal*/
+    let finDescifrado = "";/*Almacena el resultado*/
+  
+    if (toDecode == "" || offSet == "") {
+      alert("Ingresa el Texto a Cifrar o el desplazamiento");
+
+    }
+    
+    for (let i = 0; i <= toDecode.length; i++) {
+      let toAscii = toDecode.charCodeAt(i);
+  
+      if (toAscii >=65  && toAscii <= 90) {
+        codAscii = (toAscii + 65 - offSet) % 26 + 65;    
+        codDescifrado = String.fromCharCode(codAscii);
+        finDescifrado += codDescifrado;
+      } 
+      else if(toAscii >=0  && toAscii <= 64){
+        finDescifrado = "";
+        alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'");
+      }
+      else if(toAscii >=123  && toAscii <= 255){
+        finDescifrado = "";
+        alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'");
+      }
+      
+        }return finDescifrado
+   }
+  };
+
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+document.getElementById("cifrarBtn").addEventListener("click", () => {
+  const toEncode = document.getElementById("textArea1").value.toUpperCase();
+  const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
+  let result = cipher.encode(toEncode, offSet)
+  document.getElementById("textArea2").innerHTML = result;
+});
+
+document.getElementById("descifrarBtn").addEventListener("click", () => {
+  const toDecode = document.getElementById("textArea1").value.toUpperCase();
+  const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
+  let result = cipher.decode(toDecode, offSet)
+  document.getElementById("textArea2").innerHTML = result;
+});
+
+document.getElementById("resetbtn").addEventListener("click", () => {
+ location.reload(true);
+});
+
+
