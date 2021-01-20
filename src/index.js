7dc5ba1273d6ca34a71952750e9afdf7e6a57986
Katherine-fe <kattyserranobasan@gmail.com>
2021-01-20 12:16:19 -0500

    agregue una condicional para validar los espacios vacios

--- src/index.js
+++ src/index.js
@@ -7,14 +7,28 @@ document.getElementById("cifrarBtn").addEventListener("click", () => {
   const toEncode = document.getElementById("textArea1").value.toUpperCase();
   const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
   let result = cipher.encode(toEncode, offSet)
-  document.getElementById("textArea2").innerHTML = result;
+  if (result == ""){
+  
+    alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' o verifique algun campo vacio");
+  }
+  else{
+    document.getElementById("textArea2").innerHTML = result;
+  }
+
 });
 
 document.getElementById("descifrarBtn").addEventListener("click", () => {
   const toDecode = document.getElementById("textArea1").value.toUpperCase();
   const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
   let result = cipher.decode(toDecode, offSet)
-  document.getElementById("textArea2").innerHTML = result;
+  if (result == ""){
+    /*alert("Ingresa el Texto a Cifrar o el desplazamiento");*/
+    alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' o verifique algun campo vacio");
+  }
+  else{
+    document.getElementById("textArea2").innerHTML = result;
+  }
+ 
 });
 
 document.getElementById("resetbtn").addEventListener("click", () => {
