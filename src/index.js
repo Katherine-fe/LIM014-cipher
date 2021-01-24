0808d61e33ca4f3d6567602eb9afc40326dbab63
Katherine-fe <kattyserranobasan@gmail.com>
2021-01-23 21:39:59 -0500

    se redujo el nombramiento de variables y se agrego las validaciones para decode y encode

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ document.getElementById("cifrarBtn").addEventListener("click", () => {
   let toEncode = document.getElementById("textArea1").value;
   let offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
   let result = cipher.encode(offSet,toEncode);
-   document.getElementById("textArea2").innerHTML = result;
-   
- 
- });
+  if (typeof (offSet) !== 'number' || typeof (toEncode) !== 'string' || isNaN(offSet) || toEncode == "" ) {  
+  
+    alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' o verifique algun campo vacio");
+  }
+  else{
+    document.getElementById("textArea2").innerHTML = result;
+  }
+  
+});
 
  document.getElementById("descifrarBtn").addEventListener("click", () => {
   const toDecode = document.getElementById("textArea1").value;
