69230654f5cf929064c739411c819f51d9331cfd
Katherine-fe <kattyserranobasan@gmail.com>
2021-01-23 21:27:36 -0500

    se redujo nombrar variables

--- src/index.js
+++ src/index.js
@@ -3,27 +3,18 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 document.getElementById("cifrarBtn").addEventListener("click", () => {
-  const toEncode = document.getElementById("textArea1").value.toUpperCase();
+  let toEncode = document.getElementById("textArea1").value;
+  let offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
+  let result = cipher.encode(offSet,toEncode);
+   document.getElementById("textArea2").innerHTML = result;
+   
+ 
+ });
+
+ document.getElementById("descifrarBtn").addEventListener("click", () => {
+  const toDecode = document.getElementById("textArea1").value;
   const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
-  let result = cipher.encode(toEncode, offSet)
-  if (typeof (offSet) !== 'number' || typeof (toEncode) !== 'string' || isNaN(offSet) || toEncode == "" ) {  
-  
-    alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' o verifique algun campo vacio");
-  }
-  else if(result==""){
-            alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'");
-
-  }
-  else{
-    document.getElementById("textArea2").innerHTML = result;
-  }
-
-});
-
-document.getElementById("descifrarBtn").addEventListener("click", () => {
-  const toDecode = document.getElementById("textArea1").value.toUpperCase();
-  const offSet = parseInt(document.getElementById("inputOffSet").value); // Math.abs devuelve un valor absulto 
-  let result = cipher.decode(toDecode, offSet)
+  let result = cipher.decode(offSet,toDecode);
   if (typeof (offSet) !== 'number' || typeof (toDecode) !== 'string' || isNaN(offSet) || toDecode == "" ) {  
   
     alert("El texto solo debe contener 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' o verifique algun campo vacio");
@@ -36,5 +27,4 @@ document.getElementById("descifrarBtn").addEventListener("click", () => {
 document.getElementById("resetbtn").addEventListener("click", () => {
  location.reload(true);
 });
-
 
